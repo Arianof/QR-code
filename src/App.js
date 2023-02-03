fa1550f6f2e93d564da56d01a09008887489d76a
Arianof <Arian.fathi03@gmai.com>
2023-02-03 14:26:50 +0330

    feat: div tags added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+import QR from './images/image-qr-code.png'
+
+function App() {
+  return (
+      <div className='card'>
+          
+      </div>
+  );
+}
+
+export default App;
