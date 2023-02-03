0143d455960637fc856ab79bbec78fb6dc218121
Arianof <Arian.fathi03@gmai.com>
2023-02-03 14:27:38 +0330

    feat: img, h3, p tags added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import QR from './images/image-qr-code.png'
 function App() {
   return (
       <div className='card'>
-          
+          <img src={QR} alt={'qrCode'}/>
+          <h3>
+              Improve your front-end skills by building projects
+          </h3>
+          <p>
+              Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+          </p>
       </div>
   );
 }
